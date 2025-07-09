e8743fa313e9acae5401c46ecab405ef6075b8b6
Andrew882024 <acz882024@gmail.com>
2025-07-09 12:27:39 -0700

    Update utf8analyzer.c

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,6 +4,23 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//task 1
+void isValidASCII(char argv[]){
+        int lenthInput = strlen(argv);
+        int notValid = 0;
+        for(int i = 0; i<strlen(argv); i++){
+                if((argv[i]&0b11000000)==0b10000000){
+                        notValid = 1;
+                }
+        }
+        if(notValid == 1){
+                printf("Valid ASCII: false\n");
+        }
+        else{
+                printf("Valid ASCII: ture\n");
+        }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
