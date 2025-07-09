10c1dd518f8fcf83ee65df1f4d47b746dce5cbb1
Zhang <acz003@ieng6-202.ucsd.edu>
2025-07-09 12:42:24 -0700

    finish task1 and do some change on task2

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,11 +4,48 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//task 1
+void isValidASCII(char argv[]){
+	int lenthInput = strlen(argv);
+	int notValid = 0; 
+	for(int i = 0; i<strlen(argv); i++){
+		if((argv[i]&0b11000000)==0b10000000){
+			notValid = 1;
+		}
+	}
+	if(notValid == 1){
+		printf("Valid ASCII: false\n");
+	}
+	else{
+		printf("Valid ASCII: ture\n");
+	}
+}
+
+//task 2
+void makeUppercase(char argv[]){
+	char output[100] = "";
+	char temp = '';
+	for(int i = 0; i<strlen(argv); i++){
+		temp = 'a';
+		if((argv[i]<=122)&&(argv[i]>=97)){
+			temp = (argv[i] - 32);	
+		}
+		else{
+			temp = argv[i];
+		}
+		output[i] = temp; 
+	}
+	printf("u")
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
-        return 1;
-    }
+	isValidASCII("abcde");
+	isValidASCII("abcde🐿️");
+
+    //if (argc != 2) {
+    //    fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
+    //    return 1;
+    //}
 
     // implement the UTF-8 analyzer here
 }
